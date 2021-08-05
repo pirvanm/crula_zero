b34cc8acd509698aff049232be7f06208324cd3b
Pirvan Marian <pirvan.marian@gmail.com>
2021-08-05 20:29:17 +0300

    ruta pentru mail

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+Route::get('send-mail', function () {
+   
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+   
+    \Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\MyTestMail($details));
+   
+    dd("Email is Sent.");
+});
+
 require __DIR__.'/auth.php';
