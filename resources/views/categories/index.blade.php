aac4a2af231e697ebf3fb1e3c2959df33fe05f30
Pocsan Junior <pocsan95@gmail.com>
2021-08-10 00:36:29 +0300

    more layout cleaning

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,42 +7,47 @@
   }
 </style>
 <div class="uper">
+  <div class="mb-2">
+  <a href="{{ route('categories.create')}}" 
+                class="btn btn-primary">Adauga o noua categorie</a>
+  </div>
   @if(session()->get('success'))
     <div class="alert alert-success">
       {{ session()->get('success') }}  
     </div><br />
   @endif
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Created at</td>
-          <td colspan="2">Action</td>
-        </tr>
-    </thead>
-    <a href="{{ route('categories.create')}}" 
-              class="btn btn-primary">Adauga o noua categorie</a></td>
-    </div>
-    <tbody>
-        @foreach($categories as $category)
-        <tr>
-            <td>{{$category->id}}</td>
-            <td>{{$category->name}}</td>
-            <td>{{$category->created_at}}</td>
-            <td>
-              <a href="{{ route('categories.edit', $category->id)}}" 
-              class="btn btn-primary">Edit</a></td>
-            <td>
-                <form action="{{ route('categories.destroy', $category->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+  @if (count($categories))
+    <table class="table table-striped">
+      <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Created at</td>
+            <td colspan="2">Action</td>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($categories as $category)
+          <tr>
+              <td>{{$category->id}}</td>
+              <td>{{$category->name}}</td>
+              <td>{{$category->created_at}}</td>
+              <td>
+                <a href="{{ route('categories.edit', $category->id)}}" 
+                class="btn btn-primary">Edit</a></td>
+              <td>
+                  <form action="{{ route('categories.destroy', $category->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit">Delete</button>
+                  </form>
+              </td>
+          </tr>
+          @endforeach
+      </tbody>
+    </table>
+  @else 
+    <p>Hey, you have no category yet.</p>
+  @endif
 <div>
 @endsection
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -7,43 +7,47 @@
   }
 </style>
 <div class="uper">
+  <div class="mb-2">
+  <a href="{{ route('games.create')}}" 
+                class="btn btn-primary">Creaza Joc</a>
+  </div>
   @if(session()->get('success'))
     <div class="alert alert-success">
       {{ session()->get('success') }}  
     </div><br />
   @endif
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>Game Name</td>
-          <td>Game Price</td>
-          <td colspan="2">Action</td>
-        </tr>
-    </thead>
-    <div>
-    <a href="{{ route('games.create')}}" 
-              class="btn btn-primary">Creaza Joc</a></td>
-    </div>
-    <tbody>
-        @foreach($games as $game)
-        <tr>
-            <td>{{$game->id}}</td>
-            <td>{{$game->name}}</td>
-            <td>{{$game->price}}</td>
-            <td>
-              <a href="{{ route('games.edit', $game->id)}}" 
-              class="btn btn-primary">Edit</a></td>
-            <td>
-                <form action="{{ route('games.destroy', $game->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+  @if (count($games))
+    <table class="table table-striped">
+      <thead>
+          <tr>
+            <td>ID</td>
+            <td>Game Name</td>
+            <td>Game Price</td>
+            <td colspan="2">Action</td>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($games as $game)
+          <tr>
+              <td>{{$game->id}}</td>
+              <td>{{$game->name}}</td>
+              <td>{{$game->price}}</td>
+              <td>
+                <a href="{{ route('games.edit', $game->id)}}" 
+                class="btn btn-primary">Edit</a></td>
+              <td>
+                  <form action="{{ route('games.destroy', $game->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit">Delete</button>
+                  </form>
+              </td>
+          </tr>
+          @endforeach
+      </tbody>
+    </table>
+  @else
+    <p>Hey, you have no games yet.</p>
+  @endif
 <div>
 @endsection
