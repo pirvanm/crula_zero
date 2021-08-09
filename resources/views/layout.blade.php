9e8c608f788ebc13e96bcebd69b2cbd6ea543aa8
Pocsan Junior <pocsan95@gmail.com>
2021-08-09 23:40:06 +0300

    add bootstrap from cdn, not recommended

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel 8 CRUD Tutorial</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
 </head>
 <body>
   <div class="container">
     @yield('content')
   </div>
   <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
